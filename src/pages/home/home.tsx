76f26513e5d4ce975325a1ba8de1f638ffc86ab6
Alexis Jobart <alexisjobart@macbook-pro.home>
2024-10-21 18:41:36 +0200

    init project

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="w-full h-screen bg-gray-500">
+      <p>Bienvenue sur LiteMark !</p>
+    </div>
+  );
+};
+
+export default Home;
