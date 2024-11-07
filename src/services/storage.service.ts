046362a08eff072f2bbdcadfce471c704d423c1e
Alexis Jobart <jobart.pro@gmail.com>
2024-11-07 12:07:41 +0100

    LITEMARK-001 : change defaultNote

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -39,33 +39,276 @@ const defaultLongNote: Note = {
   tags: ['#tutorial'],
   lastModified: new Date(),
   content: `
-Lorem ipsum odor amet, consectetuer adipiscing elit. Blandit at adipiscing varius torquent iaculis ipsum quis nunc. Tellus facilisis quisque maximus, ac malesuada vestibulum nascetur tincidunt. Litora volutpat volutpat blandit mollis fames himenaeos parturient nascetur. Convallis montes egestas viverra porttitor rhoncus ligula varius pulvinar class. Feugiat dui ullamcorper senectus est feugiat. Convallis enim facilisi faucibus suspendisse purus nec. Montes litora malesuada viverra enim dictum praesent. Pretium nam natoque, metus molestie integer elementum.
+Create sophisticated formatting for your notes on LiteMark with simple syntax.
 
-## Second heading
-Consequat ut ut proin et donec luctus lacinia. Ridiculus ridiculus egestas dis ante convallis. Aenean leo tempus mus nulla euismod phasellus proin id. Nostra cursus dictum efficitur phasellus sapien. Nostra nisi habitasse arcu platea adipiscing; varius parturient blandit. Lectus platea class aptent diam leo nunc ultrices magna sit. Leo elit elit blandit massa, laoreet orci platea. Aliquam class volutpat egestas mi euismod tortor habitasse aliquam. Sociosqu lacus himenaeos praesent lacus tincidunt! Rhoncus porta platea platea mauris maximus malesuada.
+This is intended as a quick reference and showcase.
+For more complete info, see [John Gruber's original spec](https://daringfireball.net/projects/markdown/) and the [Github-flavored Markdown spec page](https://github.github.com/gfm/).
 
-## Third heading
-Facilisi nibh dictum sapien facilisis feugiat. Urna varius gravida sollicitudin magna netus rutrum. Aptent et felis scelerisque lacus turpis turpis imperdiet ante consectetur. Imperdiet urna pulvinar tortor euismod cursus sagittis laoreet. Consectetur fames ipsum maximus sodales donec. Faucibus senectus nam consequat odio blandit netus. Neque mattis fringilla bibendum mattis neque; sodales magna sociosqu suspendisse. Nibh tortor urna ultrices; netus convallis at eros magna.
+## Headings
 
-## Fourth heading
-Auctor semper vitae; lorem parturient nullam mattis tellus. Enim congue integer nascetur, quam bibendum fusce. Hendrerit aenean maecenas habitasse vivamus auctor amet. Nisl vulputate porta mus aliquam ornare. Montes posuere vel lectus ex massa. Aid dapibus arcu maecenas porttitor condimentum lacinia. Pharetra conubia eros fames varius lectus turpis.
+To create a heading, add one to six \`#\` symbols before your heading text. The number of \`#\` you use will determine the hierarchy level and typeface size of the heading.
 
-## Fifth heading
-Nibh hac gravida aliquet varius; sed porttitor ut. Elementum massa sed nulla dis dolor pretium. Lobortis enim turpis nostra elit eget sapien nisi. Dignissim posuere lectus luctus accumsan netus vulputate. Sem porttitor gravida est placerat parturient. Luctus bibendum mattis sociosqu sagittis sem scelerisque donec praesent potenti. Conubia morbi sed felis aliquet felis habitasse eleifend. Tristique finibus sollicitudin magna eu etiam etiam iaculis. Nunc ridiculus natoque felis ac massa maecenas ut laoreet. Conubia a aptent non vulputate fames sociosqu risus sociosqu.
+\`\`\`markdown
+# A first-level heading
 
-## Six heading
-Consequat ut ut proin et donec luctus lacinia. Ridiculus ridiculus egestas dis ante convallis. Aenean leo tempus mus nulla euismod phasellus proin id. Nostra cursus dictum efficitur phasellus sapien. Nostra nisi habitasse arcu platea adipiscing; varius parturient blandit. Lectus platea class aptent diam leo nunc ultrices magna sit. Leo elit elit blandit massa, laoreet orci platea. Aliquam class volutpat egestas mi euismod tortor habitasse aliquam. Sociosqu lacus himenaeos praesent lacus tincidunt! Rhoncus porta platea platea mauris maximus malesuada.
+## A second-level heading
 
-## Seven heading
-Facilisi nibh dictum sapien facilisis feugiat. Urna varius gravida sollicitudin magna netus rutrum. Aptent et felis scelerisque lacus turpis turpis imperdiet ante consectetur. Imperdiet urna pulvinar tortor euismod cursus sagittis laoreet. Consectetur fames ipsum maximus sodales donec. Faucibus senectus nam consequat odio blandit netus. Neque mattis fringilla bibendum mattis neque; sodales magna sociosqu suspendisse. Nibh tortor urna ultrices; netus convallis at eros magna.
+### A third-level heading
+\`\`\`
 
-## Eight heading
-Auctor semper vitae; lorem parturient nullam mattis tellus. Enim congue integer nascetur, quam bibendum fusce. Hendrerit aenean maecenas habitasse vivamus auctor amet. Nisl vulputate porta mus aliquam ornare. Montes posuere vel lectus ex massa. Aid dapibus arcu maecenas porttitor condimentum lacinia. Pharetra conubia eros fames varius lectus turpis.
+It yields:
 
-## Nine heading
-Nibh hac gravida aliquet varius; sed porttitor ut. Elementum massa sed nulla dis dolor pretium. Lobortis enim turpis nostra elit eget sapien nisi. Dignissim posuere lectus luctus accumsan netus vulputate. Sem porttitor gravida est placerat parturient. Luctus bibendum mattis sociosqu sagittis sem scelerisque donec praesent potenti. Conubia morbi sed felis aliquet felis habitasse eleifend. Tristique finibus sollicitudin magna eu etiam etiam iaculis. Nunc ridiculus natoque felis ac massa maecenas ut laoreet. Conubia a aptent non vulputate fames sociosqu risus sociosqu.
+# A first-level heading
+
+## A second-level heading
+
+### A third-level heading
+
+## Styling text
+
+You can indicate emphasis with bold, italic, strikethrough, subscript, or superscript text in comment fields and \`.md\` files.
+
+| Style                  | Syntax              | Example                                  | Output                                 |
+| ---------------------- | ------------------- | ---------------------------------------- | -------------------------------------- |
+| Bold                   | \`** **\` or \`__ __\`  |  \`**This is bold text**\`                  | **This is bold text**                  |
+| Italic                 | \`* *\` or \`_ _\`      | \`_This text is italicized_\`              | _This text is italicized_              |
+| Strikethrough          | \`~~ ~~\`             | \`~~This was mistaken text~~\`             | ~~This was mistaken text~~             |
+| Bold and nested italic | \`** **\` and \`_ _\`   | \`**This text is _extremely_ important**\` | **This text is _extremely_ important** |
+| All bold and italic    | \`*** ***\`           |  \`***All this text is important***\`       | **_All this text is important_**       |
+| Subscript              | \`<sub> </sub>\`      |  \`This is a <sub>subscript</sub> text\`    | This is a <sub>subscript</sub> text    |
+| Superscript            | \`<sup> </sup>\`      |  \`This is a <sup>superscript</sup> text\`  | This is a <sup>superscript</sup> text  |
+
+## Quoting text
+
+You can quote text with a \`>\`.
+
+\`\`\`markdown
+Text that is not a quote
+
+> Text that is a quote
+\`\`\`
+
+Quoted text is indented, with a different type color.
+
+Text that is not a quote
+
+> Text that is a quote
+
+## Quoting code
+
+You can call out code or a command within a sentence with single backticks. The text within the backticks will not be formatted.
+
+\`\`\`markdown
+Use \`git status\` to list all new or modified files that haven't yet been committed.
+\`\`\`
+
+Use \`git status\` to list all new or modified files that haven't yet been committed.
+
+To format code or text into its own distinct block, use triple backticks.
+
+\`\`\`\`
+Some basic Git commands are:
+\`\`\`
+git status
+git add
+git commit
+\`\`\`
+\`\`\`\`
+
+Some basic Git commands are:
+
+\`\`\`
+git status
+git add
+git commit
+\`\`\`
+
+## Syntax highlighting
+
+You can add an optional language identifier to enable syntax highlighting in your fenced code block.
+
+Syntax highlighting changes the color and style of source code to make it easier to read.
+
+For example, to syntax highlight Ruby code:
+
+\`\`\`\`
+\`\`\`ruby
+require 'redcarpet'
+markdown = Redcarpet.new("Hello World!")
+puts markdown.to_html
+\`\`\`
+\`\`\`\`
+
+This will display the code block with syntax highlighting:
+
+\`\`\`ruby
+require 'redcarpet'
+markdown = Redcarpet.new("Hello World!")
+puts markdown.to_html
+\`\`\`
+
+## Links
+
+You can create an inline link by wrapping link text in brackets \`[ ]\`, and then wrapping the URL in parentheses \`( )\`.
+
+\`\`\`
+This note was written using [LiteMark](https://ajobart.github.io/LiteMark/).
+\`\`\`
+
+This note was written using [LiteMark](https://ajobart.github.io/LiteMark/).
+
+## Images
+
+You can display an image by adding \`!\` and wrapping the alt text in \`[ ]\`. Alt text is a short text equivalent of the information in the image. Then, wrap the link for the image in parentheses \`()\`.
+
+\`\`\`
+![Landscape](https://images.unsplash.com/photo-1527489377706-5bf97e608852?q=80&w=3059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)
+\`\`\`
+
+![Landscape](https://images.unsplash.com/photo-1527489377706-5bf97e608852?q=80&w=3059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)
+
+## Lists
+
+You can make an unordered list by preceding one or more lines of text with \`-\`, \`*\`, or \`+\`.
+
+\`\`\`markdown
+- George Washington
+
+* John Adams
+
+- Thomas Jefferson
+\`\`\`
+
+- George Washington
+
+* John Adams
+
+- Thomas Jefferson
+
+To order your list, precede each line with a number.
+
+\`\`\`markdown
+1. James Madison
+1. James Monroe
+1. John Quincy Adams
+\`\`\`
+
+1. James Madison
+1. James Monroe
+1. John Quincy Adams
+
+### Nested Lists
+
+You can create a nested list by indenting one or more list items below another item.
+
+To create a nested list using the web editor on GitHub or a text editor that uses a monospaced font, you can align your list visually.
+Type space characters in front of your nested list item until the list marker character (\`-\` or \`*\`) lies directly below the first character of the text in the item above it.
+
+\`\`\`markdown
+1. First list item
+   - First nested list item
+     - Second nested list item
+\`\`\`
+
+1. First list item
+   - First nested list item
+     - Second nested list item
+
+For more examples, see the [GitHub Flavored Markdown Spec](https://github.github.com/gfm/#example-265).
+
+## Task lists
+
+To create a task list, preface list items with a hyphen and space followed by \`[ ]\`. To mark a task as complete, use \`[x]\`.
+
+\`\`\`markdown
+- [ ] Checkbox
+- [x] Task
+\`\`\`
+
+- [ ] Checkbox
+- [x] Task
+
+## Using emoji
+
+You can add emoji to your writing by typing \`:EMOJICODE:\`, a colon followed by the name of the emoji.
+
+\`\`\`markdown
+:white_check_mark: I've finished working on this feature. It's ready to merge! :tada:
+\`\`\`
+
+:white_check_mark: I've finished working on this feature. It's ready to merge! :tada:
+
+For a full list of available emoji and codes, see [the Emoji-Cheat-Sheet](https://github.com/wooorm/gemoji/blob/main/support.md).
+
+## Paragraphs
+
+You can create a new paragraph by leaving a blank line between lines of text.
+
+## Line Breaks
+
+To create a line break or new line (\`<br>\`), end a line with two, more spaces or a backslash (\`\\\`), and then type return.
+
+\`\`\`markdown
+This is the first line.  
+And this is the second line.\\
+This is the third line.
+\`\`\`
+
+This is the first line.  
+And this is the second line.\\
+This is the third line.
+
+## Ignoring Markdown formatting
+
+You can tell to ignore (or escape) Markdown formatting by using \`\\\` before the Markdown character.
+
+\`Let's rename \\*our-new-project\\* to \\*our-old-project\\*.\`
+
+Let's rename \\*our-new-project\\* to \\*our-old-project\\*.
+
+## Tables
+
+GFM enables the \`table\` extension, where an additional leaf block type is available.
+
+\`\`\`gfm
+Colons can be used to align columns.
+
+| Tables        |      Are      |  Cool |
+| ------------- | :-----------: | ----: |
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      |   centered    |   $12 |
+| zebra stripes |   are neat    |    $1 |
+
+There must be at least 3 dashes separating each header cell.
+The outer pipes (|) are optional, and you don't need to make the
+raw Markdown line up prettily. You can also use inline Markdown.
+
+| Markdown | Less      | Pretty     |
+| -------- | --------- | ---------- |
+| _Still_  | \`renders\` | **nicely** |
+| 1        | 2         | 3          |
+\`\`\`
+
+Here are the rendered tables:
+
+Colons can be used to align columns.
+
+| Tables        |      Are      |  Cool |
+| ------------- | :-----------: | ----: |
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      |   centered    |   $12 |
+| zebra stripes |   are neat    |    $1 |
+
+There must be at least 3 dashes separating each header cell.
+The outer pipes (|) are optional, and you don't need to make the
+raw Markdown line up prettily. You can also use inline Markdown.
+
+| Markdown | Less      | Pretty     |
+| -------- | --------- | ---------- |
+| _Still_  | \`renders\` | **nicely** |
+| 1        | 2         | 3          |
 `,
-}
+};
 
 /**
  * Function to get deleted notes from localStorage
@@ -108,8 +351,8 @@ export function initializeNotes(): void {
 
   // Only add default notes if there are no normal notes and no deleted notes
   if (notes.length === 0 && deletedNotes.length === 0) {
-    addNote(defaultNote);
     addNote(defaultLongNote);
+    addNote(defaultNote);
   }
 }
 
